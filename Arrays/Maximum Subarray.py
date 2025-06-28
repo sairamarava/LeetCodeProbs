0b50b18190a7709cab987ee85ad81a02f34a8cba
A. Sai Ram Reddy <151391940+sairamarava@users.noreply.github.com>
2025-06-28 05:43:05 +0530

    Update Maximum Subarray.py

--- Arrays/Maximum Subarray.py
+++ Arrays/Maximum Subarray.py
@@ -4,8 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #Kadanes Algorithm
         res=[]
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
                 res.append(sum(nums[i:j+1]))
         return max(res)
+
+## brute force
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = []
+        for i in range(len(nums)):
+            total = 0
+            for j in range(i, len(nums)):
+                total += nums[j]
+                res.append(total)
+        return max(res)
+
